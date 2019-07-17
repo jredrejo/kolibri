e232c053c668e4b51135963f173023bbd88bc3bf
José L. Redrejo Rodríguez <jredrejo@debian.org>
2019-07-17 19:24:30 +0200

    Adding a hack for unwomen

--- kolibri/plugins/oidc_provider_plugin/__init__.py
+++ kolibri/plugins/oidc_provider_plugin/__init__.py
@@ -1,6 +1,13 @@
+import os
+
+
 def kolibri_userinfo(claims, user):
     """
     Fill claims with the information available in the Kolibri database
     """
+
     claims["name"] = user.full_name
+    COUNTRY = os.environ.get("COUNTRY", None)
+    if COUNTRY:
+        claims["email"] = "{username}@{country}.org".format(username=user.username, country=COUNTRY)
     return claims
